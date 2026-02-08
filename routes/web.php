307e8c7e0389dd50df341cf6db51d9394ab91d08
nurulhidayah-sfw <nurulhidayahlop@gmail.com>
2026-02-08 09:58:51 +0700

    Add books feature (model, migration, controller, routes)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/create', [BookController::class, 'create']);
+Route::post('/books', [BookController::class, 'store']);
