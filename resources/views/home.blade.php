c352ca6af5e6f789ebe837f0d0d11b087d899b94
Fitri <Fitriromadhani.nfr@gmail.com>
2026-02-09 18:35:32 +0700

    Menambahkan HomeController dan home.blade.php, route Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Home - Perpustakaan</title>
+</head>
+<body>
+    <h1>Selamat datang di Perpustakaan</h1>
+    <p>Ini halaman Home saja.</p>
+</body>
+</html>
